fc83f930cad2f3f5c023e477f2de268cc4533c85
aylutequiero <vazquezfagustin@gmail.com>
2019-08-22 23:02:03 -0300

    test2.0

---
+++ test/info/users.js
@@ -0,0 +1,10 @@
+const users = [
+        {
+            username:"vazquezag",
+            password:"4159"
+        },
+        {
+            username: 'aylu',
+            password: 'aylu777'
+        }
+    ];
